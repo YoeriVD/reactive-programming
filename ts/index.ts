48aa48bcc283050fb00fa01025434458b96ba0fa
YoeriVD <yoeri.vandamme@gmail.com>
2017-09-13 18:02:31 +0200

    jquery klaargezet

--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,12 @@
-console.log('hi');
+import Rx from "rxjs/Rx";
+import $ from "jquery";
+
+$('h3').html("Status: ready!");
+
+const button : HTMLButtonElement = $('button')[0];
+const text : HTMLInputElement = $('input')[0];
+
+let clickStream = Rx.Observable.fromEvent(button, 'click')
+clickStream.subscribe(e => {
+    text.value = 'clicked!'
+});
