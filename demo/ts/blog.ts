a531d3fa08d630bb79e42c7ecf97468744ee5e60
YoeriVD <yoeri.vandamme@gmail.com>
2017-09-18 10:49:07 +0200

    demo klaarzetten

--- demo/ts/blog.ts
+++ demo/ts/blog.ts
@@ -46,3 +46,33 @@ function renderItem(
 }
 
 // streams..
+const keyup$ =
+Rx.Observable.fromEvent(input, 'keyup')
+    .debounceTime(250)
+    .map((e: KeyboardEvent) => (<HTMLInputElement>e.target).value)
+    .filter(val => val.length > 2);
+
+keyup$
+.switchMap(val => {
+  const data = fetch(postsUrl).then(p => p.json());
+  const returnValue = data.then(p => ({filter: val, data: p}));
+  return Rx.Observable.fromPromise(returnValue);
+})
+.map(result => {
+  if (!result.filter) {
+    return result;
+  }
+  return {
+    filter: result.filter,
+        data: result.data.filter(
+            post => post.title.indexOf(result.filter) > -1 ||
+                post.body.indexOf(result.filter) > -1)
+  }
+})
+.subscribe(result => {
+  postList.innerHTML = '';
+
+  result.data.forEach(post => {
+    const listItem = renderItem(postList, post, result.filter);
+  })
+});
